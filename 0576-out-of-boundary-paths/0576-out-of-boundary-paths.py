1711214bf938c0e476fbd393e6b0ae166c790877
Divya <divyasreem48@gmail.com>
2024-01-26 21:57:45 +0530

    Time: 105 ms (53.47%), Space: 20 MB (65.90%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -7,34 +7,28 @@ class Solution:
                 return 0
             if i<0 or i>=m or j<0 or j>=n:
                 return 1
-            if dp[i+1][j][k-1]!=-1:
-                a=dp[i+1][j][k-1]
-            else:
-                dp[i+1][j][k-1]=fun(i+1,j,m,n,k-1)
-                a=dp[i+1][j][k-1]
+            if dp[i][j][k]!=-1:
+                return dp[i][j][k]
+            
+            dp[i+1][j][k-1]=fun(i+1,j,m,n,k-1)
+            a=dp[i+1][j][k-1]
+
+
+
+            dp[i-1][j][k-1]=fun(i-1,j,m,n,k-1)
+            b=dp[i-1][j][k-1]
+
+
+
+            dp[i][j+1][k-1]=fun(i,j+1,m,n,k-1)
+            c=dp[i][j+1][k-1]
+
+
+
+            dp[i][j-1][k-1]=fun(i,j-1,m,n,k-1)
+            d=dp[i][j-1][k-1]
                 
-                
-            if dp[i-1][j][k-1]!=-1:
-                b=dp[i-1][j][k-1]
-            else:
-                dp[i-1][j][k-1]=fun(i-1,j,m,n,k-1)
-                b=dp[i-1][j][k-1]
-                
-                
-            if dp[i][j+1][k-1]!=-1:
-                c=dp[i][j+1][k-1]
-            else:
-                dp[i][j+1][k-1]=fun(i,j+1,m,n,k-1)
-                c=dp[i][j+1][k-1]
-                
-                
-            if dp[i][j-1][k-1]!=-1:
-                d=dp[i][j-1][k-1]
-            else:
-                dp[i][j-1][k-1]=fun(i,j-1,m,n,k-1)
-                d=dp[i][j-1][k-1]
-                
-                
-            return sum([a,b,c,d])%mod
+            dp[i][j][k]=sum([a,b,c,d])  
+            return dp[i][j][k]%mod
         return fun(row,col,m,n,maxMove)%mod
         
