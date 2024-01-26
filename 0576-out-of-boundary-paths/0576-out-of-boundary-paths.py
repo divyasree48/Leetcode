794a1295f72bed6ced335f389e040b6d0ba605db
Divya <divyasreem48@gmail.com>
2024-01-26 21:37:59 +0530

    Time: 85 ms (75.43%), Space: 20.1 MB (65.61%) - LeetHub

---
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, row: int, col: int) -> int:
+        mod=1000000007
+        dp=[[[-1 for i in range(maxMove+1)]for j in range(n+1)]for _ in range(m+1)]
+        def fun(i,j,m,n,k):
+            if k<0:
+                return 0
+            if i<0 or i>=m or j<0 or j>=n:
+                return 1
+            if dp[i+1][j][k-1]!=-1:
+                a=dp[i+1][j][k-1]
+            else:
+                dp[i+1][j][k-1]=fun(i+1,j,m,n,k-1)
+                a=dp[i+1][j][k-1]
+                
+                
+            if dp[i-1][j][k-1]!=-1:
+                b=dp[i-1][j][k-1]
+            else:
+                dp[i-1][j][k-1]=fun(i-1,j,m,n,k-1)
+                b=dp[i-1][j][k-1]
+                
+                
+            if dp[i][j+1][k-1]!=-1:
+                c=dp[i][j+1][k-1]
+            else:
+                dp[i][j+1][k-1]=fun(i,j+1,m,n,k-1)
+                c=dp[i][j+1][k-1]
+                
+                
+            if dp[i][j-1][k-1]!=-1:
+                d=dp[i][j-1][k-1]
+            else:
+                dp[i][j-1][k-1]=fun(i,j-1,m,n,k-1)
+                d=dp[i][j-1][k-1]
+                
+                
+            return sum([a,b,c,d])%mod
+        return fun(row,col,m,n,maxMove)%mod
+        
